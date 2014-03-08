2565af4e04854dc168428405ba8a9ea12f3b1973
Jacob Schellenberg <schellenberghq@gmail.com>
2014-03-08 10:20:24 -0700

    Updated GameOver and Tutorial.

---
+++ Monster Smash/Assets/Monster Smash/Source/ViewControllers/GameOverViewController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverViewController : MonoBehaviour {
+
+	[SerializeField]
+	private UILabel highScore;
+	[SerializeField]
+	private UILabel currentScore;
+	[SerializeField]
+	private UILabel newHighScore;
+
+	void Start(){
+		highScore.text = PlayerPrefs.GetInt("HighScore").ToString();
+		currentScore.text = PlayerPrefs.GetInt("CurrentScore").ToString();
+		newHighScore.gameObject.SetActive(false);
+		if(PlayerPrefs.GetInt("CurrentScore") >= PlayerPrefs.GetInt("HighScore"))
+			newHighScore.gameObject.SetActive(true);
+	}
+
+	public void OnReplayButtonClicked(){
+		PlayerPrefs.SetInt("CurrentScore", 0);
+		Application.LoadLevelAsync("Game");
+	}
+}
