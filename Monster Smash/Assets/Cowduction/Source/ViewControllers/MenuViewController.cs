055be2d74909770f4a004384b0bca49f871e47c5
Jacob Schellenberg <schellenberghq@gmail.com>
2014-03-23 12:13:50 -0700

    Added start sequence animation when you push play.

--- Monster Smash/Assets/Cowduction/Source/ViewControllers/MenuViewController.cs
+++ Monster Smash/Assets/Cowduction/Source/ViewControllers/MenuViewController.cs
@@ -3,7 +3,46 @@ using System.Collections;
 
 public class MenuViewController : MonoBehaviour {
 
+	public UISprite cow;
+	public UISprite absorbBeam;
+	public UISprite spaceShip;
+
+	void Start(){
+		cow.gameObject.SetActive(true);
+		cow.GetComponent<TweenPosition>().enabled = false;
+		cow.GetComponent<TweenRotation>().enabled = false;
+		cow.GetComponent<TweenAlpha>().enabled = false;
+		absorbBeam.gameObject.SetActive(false);
+		absorbBeam.alpha = 0;
+		spaceShip.gameObject.SetActive(false);
+	}
+
 	public void OnPlayButtonClicked(){
+		StartCoroutine(PlaySequence());
+	}
+
+	IEnumerator PlaySequence(){
+		absorbBeam.gameObject.SetActive(true);
+		spaceShip.gameObject.SetActive(true);
+
+		absorbBeam.GetComponent<TweenAlpha>().ResetToBeginning();
+		absorbBeam.GetComponent<TweenAlpha>().PlayForward();
+		
+		spaceShip.GetComponent<TweenPosition>().ResetToBeginning();
+		spaceShip.GetComponent<TweenPosition>().PlayForward();
+
+		cow.GetComponent<TweenPosition>().enabled = true;
+		cow.GetComponent<TweenRotation>().enabled = true;
+		cow.GetComponent<TweenAlpha>().enabled = true;
+		
+		cow.GetComponent<TweenPosition>().ResetToBeginning();
+		cow.GetComponent<TweenRotation>().ResetToBeginning();
+
+		cow.GetComponent<TweenPosition>().PlayForward();
+		cow.GetComponent<TweenPosition>().PlayForward();
+
+		yield return new WaitForSeconds(3);
 		Application.LoadLevelAsync("Game");
 	}
+	
 }
